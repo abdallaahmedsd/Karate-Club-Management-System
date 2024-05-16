64c5bcaa7760d642025eaa8900b11a5d62a380c8
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2024-05-17 00:58:49 +0300

    Business Layer Code

---
+++ Karate_Club_Business/Person.cs
@@ -0,0 +1,115 @@
+﻿using Karate_Club_Data_Access;
+using System;
+using System.Data;
+
+namespace Karate_Club_Business
+{
+    public class clsPerson
+    {
+        public int PersonID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName 
+        {   
+            get { return FirstName + ' ' + LastName; }
+        }
+        public char Gender { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string ImagePath { get; set; }
+        public int? CreatedByUserID { get; set; }
+        public enum enMode { add_new_mode, update_mode }
+        public enMode Mode {get; set;}
+
+        private clsPerson(int personID, string firstName, string lastName, char gender, DateTime? birthdate, string phone, string email, string address, string imagePath, int? createdByUserID)
+        {
+            PersonID = personID;
+            FirstName = firstName;
+            LastName = lastName;
+            Gender = gender;
+            Birthdate = birthdate;
+            Phone = phone;
+            Email = email;
+            Address = address;
+            ImagePath = imagePath;
+            CreatedByUserID = createdByUserID;
+            Mode = enMode.update_mode;
+        }
+
+        public clsPerson()
+        {
+            PersonID = -1;
+            FirstName = null;
+            LastName = null;
+            Gender = ' ';
+            Birthdate = null;
+            Phone = null;
+            Email = null;
+            Address = null;
+            ImagePath = null;
+            CreatedByUserID = null;
+            Mode = enMode.add_new_mode;
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.add_new_mode:
+                    if (_AddPerson())
+                    {
+                        this.Mode = enMode.update_mode;
+                        return true;
+                    }
+                    return false;
+                case enMode.update_mode:
+                    return _UpdatePerson();
+                default:
+                    return false;
+            }
+        }
+
+        public static clsPerson Find(int personID)
+        {
+            string firstName = null,lastName = null, phone = null, email = null, address = null, imagePath = null;
+            char gender = ' ';
+            DateTime? birthdate = null;
+            int? createdByUserID = null;
+
+            if (clsPersonDataAccess.GetPersonByID(personID, ref firstName, ref lastName, ref gender, ref birthdate, ref phone, ref email, ref address, ref imagePath, ref createdByUserID))
+                return new clsPerson(personID, firstName, lastName, gender, birthdate, phone, email, address, imagePath, createdByUserID);
+            else
+                return null;
+
+        }
+
+        public static bool Delete(int personID)
+        {
+            return clsPersonDataAccess.DeletePerson(personID);
+        }
+
+        public static bool IsExists(int personID)
+        {
+            return clsPersonDataAccess.IsPersonExists(personID);
+        }
+
+        public static DataTable GetAllPeople()
+        {
+            return clsPersonDataAccess.GetAllPeople();
+        }
+
+        private bool _AddPerson()
+        {
+            this.PersonID = clsPersonDataAccess.AddPerson(FirstName, LastName, Gender, Birthdate, Phone, Email, Address, ImagePath, CreatedByUserID);
+            return this.PersonID != -1;
+        }
+
+        private bool _UpdatePerson() 
+        {
+            return clsPersonDataAccess.UpdatePerson(PersonID, FirstName, LastName, Gender, Birthdate, Phone, Email, Address, ImagePath);
+        }
+
+    }
+}
---
+++ Karate_Club_Business/User.cs
@@ -0,0 +1,95 @@
+﻿using Karate_Club_Data_Access;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karate_Club_Business
+{
+    public class clsUser
+    {
+        public int UserID { get; set; }
+        public int PersonID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+        public int CreatedByUserID { get; set; }
+
+        public enum enMode { AddNew, Update }
+        public enMode Mode { get; set; }
+
+        private clsUser(int userID, int personID, string userName, string password, bool isActive, int createdByUserID)
+        {
+            UserID = userID;
+            PersonID = personID;
+            UserName = userName;
+            Password = password;
+            IsActive = isActive;
+            CreatedByUserID = createdByUserID;
+            Mode = enMode.Update;
+        }
+
+        public clsUser()
+        {
+            UserID = -1;
+            PersonID = -1;
+            UserName = null;
+            Password = null;
+            IsActive = true;
+            CreatedByUserID = -1;
+            Mode = enMode.AddNew;
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddUser())
+                    {
+                        this.Mode = enMode.Update;
+                        return true;
+                    }
+                    return false;
+                case enMode.Update:
+                    return _UpdateUser();
+                default:
+                    return false;
+            }
+        }
+
+        public static clsUser Find(int userID)
+        {
+            int personID = -1, createdByUserID = -1;
+            string userName = null, password = null;
+            bool isActive = false;
+
+            if (clsUserDataAccess.GetUserByID(userID, ref personID, ref userName, ref password, ref isActive, ref createdByUserID))
+                return new clsUser(userID, personID, userName, password, isActive, createdByUserID);
+            else
+                return null;
+        }
+
+        public static bool Delete(int userID)
+        {
+            return clsUserDataAccess.DeleteUser(userID);
+        }
+
+        public static bool IsExists(int userID)
+        {
+            return clsUserDataAccess.IsUserExists(userID);
+        }
+
+        private bool _AddUser()
+        {
+            this.UserID = clsUserDataAccess.AddUser(PersonID, UserName, Password, IsActive, CreatedByUserID);
+            return this.UserID != -1;
+        }
+
+        private bool _UpdateUser()
+        {
+            return clsUserDataAccess.UpdateUser(UserID, PersonID, UserName, Password, IsActive);
+        }
+    }
+}
