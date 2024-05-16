c0c6a3fd63e9f4996d01499834628050e81b77ea
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2024-05-17 01:03:44 +0300

    The presentation layer, adding the main form

---
+++ Karate_Club/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Karate_Club
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Test());
+            Application.Run(new Karate_Club());
+        }
+    }
+}
